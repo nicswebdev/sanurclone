9c75c2c95ffc86aeb5f5ecb549ecd796d0f1feb9
nicswebdev <nicholaspratama94s@gmail.com>
2022-11-08 17:49:37 +0800

    reduce

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        largePageDataBytes: 256 * 100000,
-    },
 };
 
 module.exports = {
     images: {
         domains: ["phpstack-841991-2998353.cloudwaysapps.com"],
     },
-    nextConfig,
 };
