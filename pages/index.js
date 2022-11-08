0429e3d2348d5f1c2d11d8ff190904565ae4ed7c
nicswebdev <nicholaspratama94s@gmail.com>
2022-11-08 18:02:44 +0800

    change id

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home({
     villasData,
     experiencesData,
     offersData,
-    reviewsData,
 }) {
     const responsive = {
         superLargeDesktop: {
@@ -431,7 +430,7 @@ export async function getServerSideProps() {
             villasData,
             experiencesData,
             offersData,
-            reviewsData,
+            // reviewsData,
         },
     };
 }
